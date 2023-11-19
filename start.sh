18edab12216cc9986c8cf6dcf2fc0070840dc0c0
Denizly Furqan <31403046+zycoder0day@users.noreply.github.com>
2023-11-19 23:34:07 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-./t-rex -a kawpow -o kp.unmineable.com:3333 -u TRX:TBgBmWbraHsUDqsPvNGVEf6Z5Co9d4h2YX.WORKER1#o7lq-ygj4 -p x
+./t-rex -a kawpow -o kp.unmineable.com:3333 -u TRX:TUqPRwoCBj7mf9ZM5c4ggzUGfX5YNCWXWe.WORKER1#o7lq-ygj4 -p x
 pause
